41277c8ea5343dd51291314b6e22e3e61585970c
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-21 13:13:13 +0300

    bug fix

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
-trait GetInfo{
-    fn get_info(&self)->String;
+use std::fs::Metadata;
+
+trait GetInfo {
+    fn get_info(&self) -> String;
 }
 
-struct Massage{
-    author:String,
-    text:String
+struct Message {
+    author: String,
+    text: String,
 }
-struct Post{
-    author:String,
-    contents:String,
-    likes:i32
+
+struct Post {
+    author: String,
+    contents: String,
+    likes: i32,
 }
-impl GetInfo for Massage{
+
+impl GetInfo for Message {
     fn get_info(&self) -> String {
-        format!("Message author is {}\nMessage text:{}",self.author,self.text)
+        format!("Message author is {}\nMessage text:{}", self.author, self.text)
     }
 }
 
-impl GetInfo for Post{
+impl GetInfo for Post {
     fn get_info(&self) -> String {
-        format!("Post author:{}\nPost content:{}\nPost got {} likes!",self.author,self.contents,self.likes)
+        format!("Post author:{}\nPost content:{}\nPost got {} likes!", self.author, self.contents, self.likes)
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let message = Message {
+        author: "Mega".to_string(),
+        text: "Hello guys , welcome my git repository".to_string(),
+    };
+    let post = Post{
+        author: "Michel".to_string(),
+        contents: "Heyyy yooo welcome my rust learning git repository".to_string(),
+        likes: 4,
+    };
+    println!("{}",message.get_info());
+    println!("{}",post.get_info());
 }
